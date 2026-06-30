5f4c78eeccd08d80eda91d4ff51ccac5246a4c78
Yasi Ayazi <yasi.ayazi@gmail.com>
2026-06-30 14:02:07 +0200

    "Initial project setup"

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Home Finder</h1>
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
