54af0bd22f11f0ce06d0e1be54d1009fce110140
Yasi Ayazi <yasi.ayazi@gmail.com>
2026-07-08 17:36:17 +0200

    Add Hero component with styles and structure for the hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero__content">
+
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
