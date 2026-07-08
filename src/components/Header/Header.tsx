022e9d154bc7535a7697feddfe2bc366e0aebddc
Yasi Ayazi <yasi.ayazi@gmail.com>
2026-07-08 17:35:56 +0200

    Refactor Header component structure and styles for better layout and alignment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,20 +4,22 @@ function Header() {
   return (
 
     <header className="site-header">
-      <a href="/" className="site-logo">
-        HomeFinder
-      </a>
-    
-    <nav className="header-nav">
-      <ul className="header-menu">
-        <li><a href="/home">Home</a></li>
-        <li><a href="/buy">Buy</a></li>
-        <li><a href="/rent">Rent</a></li>
-        <li><a href="/favorites">Favorites</a></li>
-        <li><a href="/contact">Contact Us</a></li>
-      </ul>
-    </nav>
-    </header>    
+      <div className="container">
+        <a href="/" className="site-logo">
+          HomeFinder
+        </a>
+
+        <nav className="header-nav">
+          <ul className="header-menu">
+            <li><a href="/home">Home</a></li>
+            <li><a href="/buy">Buy</a></li>
+            <li><a href="/rent">Rent</a></li>
+            <li><a href="/favorites">Favorites</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
 
