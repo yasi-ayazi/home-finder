b86e935a462b6d361038497e712ffd75db6a6a92
Yasi Ayazi <yasi.ayazi@gmail.com>
2026-07-09 17:49:20 +0200

    Refactor Hero component styles and structure for improved layout and design

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import "./Hero.css";
+import SearchPanel from "../SearchPanel/SearchPanel";
 
 function Hero() {
     return (
         <section className="hero">
             <div className="hero__content">
+                <SearchPanel />
 
             </div>
         </section>
